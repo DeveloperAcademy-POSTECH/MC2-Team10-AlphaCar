370e1305fdfe97e531c77c53a6e53c3efd0a761e
ngbeen <44664457+ngbeen@users.noreply.github.com>
2023-05-14 00:30:51 +0900

    feat: 로티, 캐릭터뷰, 엔딩뷰 - 로티파일 추가, 로티설정 수정, 캐릭터뷰/엔딩뷰
    - 로티파일(great, happy, love)를 추가했습니다.
    - 로티 loop가 필요해서 매개변수 .loop를 사용할 수 있도록 수정했습니다.(기본 .playOnce)
    - 캐릭터뷰/엔딩뷰를 만들었습니다.
    - 빨간색으로 실행안되는 코드 및 파일들 지웠는데 이거 괜찮나요?

--- SagakSagak/View/Character/CharacterView1.swift
+++ SagakSagak/View/Character/CharacterView1.swift
@@ -11,13 +11,52 @@ struct CharacterView1: View {
     @EnvironmentObject private var coordinator: Coordinator
     
     var body: some View {
-        VStack{
-            Text("캐릭터")
-            Button("화면"){
-                coordinator.push(.character2)
+
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                GeometryReader { geometry in
+                    ZStack {
+                        Ellipse()
+                            .foregroundColor(.bg4)
+                            .frame(width: 1000, height: 400)
+                            .offset(y: 100)
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    
+                    HStack{
+                        Spacer()
+                        GLBavBarItem(navBarTitle: "정말 멋진 그림이야!", navBarBgColor: .system2, navBarFontColor: .system3)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    .padding(.trailing, 24)
+                    .padding(.top, 18)
+                    
+                VStack{
+                    LottieView(jsonName: "great", loopMode: .loop)
+                        .frame(height: 330)
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.bottom, geometry.safeAreaInsets.bottom)
+                    
+                ZStack{
+                        GLButtonSet(page: .character1, backButtonImage: "button_back", forwardButtonImage: "button_next")
+                        
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.trailing, 24)
+                    .padding(.bottom, 18)
+
+                }
             }
-        }.navigationBarBackButtonHidden(true)
-    }
+            .background(Color.bg2)
+            .ignoresSafeArea()
+        }
+
 }
 
 struct CharacterView1_Previews: PreviewProvider {
--- SagakSagak/View/Character/CharacterView2.swift
+++ SagakSagak/View/Character/CharacterView2.swift
@@ -11,13 +11,53 @@ struct CharacterView2: View {
     @EnvironmentObject private var coordinator: Coordinator
     
     var body: some View {
-        VStack{
-            Text("캐릭터2")
-            Button("화면"){
-                coordinator.push(.face)
+
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                GeometryReader { geometry in
+                    ZStack {
+                        Ellipse()
+                            .foregroundColor(.bg4)
+                            .frame(width: 1000, height: 400)
+                            .offset(y: 100)
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    
+                    HStack{
+                        Spacer()
+                        GLBavBarItem(navBarTitle: "소중한 것을 떠올리면 어떤 표정이 될까?", navBarBgColor: .system2, navBarFontColor: .system1)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    .padding(.trailing, 24)
+                    .padding(.top, 18)
+                    
+                VStack{
+                    LottieView(jsonName: "happy", loopMode: .loop)
+                        .frame(height: 360)
+                        .offset(y: 20)
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.bottom, geometry.safeAreaInsets.bottom)
+                    
+                ZStack{
+                        GLButtonSet(page: .character2, backButtonImage: "button_back", forwardButtonImage: "button_next")
+                        
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.trailing, 24)
+                    .padding(.bottom, 18)
+
+                }
             }
-        }.navigationBarBackButtonHidden(true)
-    }
+            .background(Color.bg2)
+            .ignoresSafeArea()
+        }
+
 }
 
 struct CharacterView2_Previews: PreviewProvider {
