8d65f6b7ba4317eb682c21da8a26e95ba2d6baf8
JOY <jypjoy@gmail.com>
2023-05-14 15:03:52 +0900

    ✨ feat: ViewTemplate 짬

--- SagakSagak/SagakSagak/DesignSystem/Component/GLBasicView_type1.swift
+++ SagakSagak/SagakSagak/DesignSystem/Component/GLBasicView_type1.swift
@@ -10,47 +10,43 @@ import SwiftUI
 struct GLBasicView_type1: View {
     let navBarTitle: String
     let navBarBgColor: Color
-    let navBarFontColor: Color
-    let customView: any View
+    let navBarFontcolor: Color
     
     var body: some View {
-        
-        ZStack {
-            Image("background")
-                .resizable()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-            GeometryReader { geometry in
-                GLNavBarItem(navBarTitle: "가람이에게 가장 소중한 것을 그려보자!", navBarBgColor: navBarBgColor, navBarFontColor: navBarFontColor)
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
+                GeometryReader { geometry in
+    
+                    GLNavBarItem(navBarTitle: navBarTitle, navBarBgColor: navBarBgColor, navBarFontColor: navBarFontcolor)
+               
                 VStack{
-                    //TODO 커스텀 뷰 여기에 추가
+
+                    //TODO: viewBuilder 통해 해당 뷰들이 여기 들어가도록 짜기 
                     
+                    Rectangle()
+                        .frame(height:88)
+                        .overlay{
+                            GLBlockView()
+                        }
+                        .foregroundColor(.white)
                     
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.bottom, geometry.safeAreaInsets.bottom)
                 }
-                .navigationBarBackButtonHidden(true)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-                .padding(.bottom, geometry.safeAreaInsets.bottom)
-                
-                ZStack{
-                    GLButtonSet(nextpage: .end3, backButtonImage: "button_back", forwardButtonImage: "button_next")
-                    
-                }
-                .navigationBarBackButtonHidden(true)
-                
             }
+            .background(Color.bg2)
+            .ignoresSafeArea()
         }
-        .background(Color.bg2)
-        .ignoresSafeArea()
-        
-        
-    }
+    
 }
 
-
-
-struct GLBasicView_type1_Previews: PreviewProvider {
-    static var previews: some View {
-        GLBasicView_type1(navBarTitle: "가람이에게 가장 소중한 것을 그려보자!", navBarBgColor: .system2, navBarFontColor: .system1, customView: Rectangle())
-    }
-}
+//struct GLBasicView_type1_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GLBasicView_type1(navBarTitle: <#String#>, navBarBgColor: <#Color#>, navBarFontcolor: <#Color#>)
+//    }
+//}
