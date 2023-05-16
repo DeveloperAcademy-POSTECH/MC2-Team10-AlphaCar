24c6052561e8df42b7941df8fbc340b2d4b497d4
JOY <jypjoy@gmail.com>
2023-05-16 17:20:07 +0900

    ✨ feat: 토깽이 뷰 완성

--- SagakSagak/View/FaceExpress/ExpressView.swift
+++ SagakSagak/View/FaceExpress/ExpressView.swift
@@ -17,44 +17,33 @@ struct ExpressView: View {
     
     
     var body: some View {
+        
+        
         if !isNextBtnClicked && !isPrevBtnClicked && !isExitBtnClicked{
             ZStack {
-                Image("background").resizable().scaledToFill()
-                Color.bg2.ignoresSafeArea().opacity(0.5)
-                VStack {
-                    ZStack {
-                        Image("text").frame(alignment: .center)
-                        Image("button_exit").offset(x:380)
-                            .onTapGesture {
-                                coordinator.popToRoot()
-                            }
-                    }
+                Color.bg2.ignoresSafeArea()
+                Image("background").resizable().ignoresSafeArea()
+                    LottieView(jsonName: "basic")
+                        .frame(width:370, height:370)
+                        .padding(.top, 40)
+                        //.background(.yellow)
                     
-                    HStack{
-                        Image("button_back")
-                            .onTapGesture {
-                                isPrevBtnClicked = true
-                            }
-                        if let image = emotionFace.faceName {
-                            LottieView(jsonName: image)
-                                .id(image)
-                      
-                        }else{
-                            LottieView(jsonName: "basic")
+                    ZStack{
+//                        navBarView
+//                        FaceView()
+                        VStack{
+                            navBarView
+                            Spacer().frame(height:230)
+                            FaceView()
+                            
                         }
-                        Image("button_next")
-                            .onTapGesture {
-                                if selectedFace != "basic" {
-                                    isNextBtnClicked = true
-                                    coordinator.push(.emotion)
-                                }
-                            }
+                        
+                        
                     }
-                }
+        
                 .padding()
-                FaceView()
-                    .environmentObject(self.emotionFace)
-                    .offset(y:150)
+       
+                    //.offset(y:150)
             }
             .onChange(of: emotionFace.faceName) { _ in
                 selectedFace = emotionFace.faceName ?? "basic"
@@ -66,8 +55,35 @@ struct ExpressView: View {
             EmotionView(onClicked: false, textBlock: "감정").environmentObject(self.emotionFace)
                 .navigationBarBackButtonHidden(true)
         }
+        
+        
 
     }
+    
+    
+    var navBarView: some View {
+        HStack{
+            GLNavBarItem(backPage: .face, backButtonImg: "button_back", shadowOn: true, navBarTitle: "소중한 것을 떠올리면 어떤 표정이 될까", navBarBgColor: .system2, navBarFontColor: .system1, nextButtonImg: "button_next", nextPage: .story1, nextEnabled: false) //변수
+            
+            Spacer().frame(width:70)
+            
+            Button {
+                coordinator.popToRoot()
+            } label: {
+                Image("button_exit")
+                    .shadow(color: Color(hex: "26775F").opacity(0.15),
+                            radius: 30,
+                            x: 0,
+                            y: 4)
+                
+            }
+        }
+        .frame(width: Const.glScreenWidth, height: 72)
+        .padding(.top, 24)
+        .padding(.trailing, 24)
+        .padding(.leading, 142)
+    }
+    
 }
 
 struct ExpressView_Previews: PreviewProvider {
